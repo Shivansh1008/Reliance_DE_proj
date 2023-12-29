431822c2ad777c03a45437562474f51867f020cf
Shivansh1008 <119164017+Shivansh1008@users.noreply.github.com>
2023-12-30 03:00:29 +0530

    Update config.py

--- resources/dev/config.py
+++ resources/dev/config.py
@@ -6,8 +6,8 @@ salt = "youtube_AesEncryption"
 
 #AWS Access And Secret key
 region_name='Asia Pacific (Mumbai) ap-south-1'
-aws_access_key = "AKIA6I3N3W56ACD2IVD4"
-aws_secret_key = "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"
+aws_access_key = ""
+aws_secret_key = ""
 bucket_name = "reliance-mart-de-project"
 s3_customer_datamart_directory = "customer_data_mart"
 s3_sales_datamart_directory = "sales_data_mart"
